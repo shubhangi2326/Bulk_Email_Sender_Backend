f0b16c9552ec7b770e3d412b98a31748d1f6d88f
shubhangi2326 <shubhangimahajan2311@gmail.com>
2025-09-24 15:33:21 +0530

    fix: updated multer destination for Vercel

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 import { handleUploadAndSend } from '../controllers/uploadController.js'; 
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: '/tmp' });
 
 router.post('/upload-and-send', upload.single('excelFile'), handleUploadAndSend);
 
